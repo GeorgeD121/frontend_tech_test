0c6c9020aaf7b6b3040fc7f607f18917ef0f92d7
GeorgeD121 <georgedunning@georges-mbp.home>
2014-09-05 13:52:15 +0100

    added menu scroll animation

--- js/main.js
+++ js/main.js
@@ -22,6 +22,24 @@ $(document).ready(function() {
   $('.content_grid').pajinate({
       items_per_page: 6
   });
+
+  jQuery(document).ready(function(){
+    var homeTopPosition = jQuery('#hero').offset().top;
+    var aboutTopPosition = jQuery('.about_us').offset().top;
+    var contentTopPosition = jQuery('.content_grid').offset().top;
+    jQuery('#home_nav').click(function(){
+        jQuery('html, body').animate({scrollTop:homeTopPosition}, 'slow');
+        return false;
+    });
+    jQuery('#about_nav').click(function(){
+        jQuery('html, body').animate({scrollTop:aboutTopPosition}, 'slow');
+        return false;
+    });
+    jQuery('#contact_nav').click(function(){
+        jQuery('html, body').animate({scrollTop:contentTopPosition}, 'slow');
+        return false;
+    });
+  });
 });
 
 
